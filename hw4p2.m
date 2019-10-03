22275eedebd0a91c5f98461350c79648144b8787
warmflasha <warmflasha@gmail.com>
2019-10-03 12:19:46 -0500

    Added part 2.

---
+++ hw4p2.m
@@ -0,0 +1,16 @@
+%2. - Do the inclass assignment (inclass 10). Here only include your
+%answers to the questions a and b there. 
+
+
+%3. - Use the data from part 1 of this homework. Pool all the data into one
+% large matrix regardless of the labelled cell type. 
+%A. Run PCA on your dataset. (a) Make plots of PC1 vs PC2, PC1 vs PC3, and
+% PC2 vs PC3 in which each point is a different cell. 
+% B. Run k-means clustering on this dataset. Try different numbers of
+% clusters ranging from 3 to 50 (you don't need to try every number, just 
+% sample from this range). In each case, make a plot of PC1 vs PC2 where
+% the points are color-coded by cluster. What number of clusters best
+% represents the data? Why? 
+% C. Make a plot of PC1 vs PC2 but this type color-coded by the annotated
+% cell types from the paper. Comment on how this compares to your plots
+% from B. 
